48805e19676284128e8720a61e053f8b6ed4c347
jy <juyoun919@gmail.com>
2025-04-06 01:36:52 +0900

    back + front 구조 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/hello", {
+      withCredentials: true
+    })
+    .then(res => setMsg(res.data))
+    .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{msg}</h1>
     </div>
   );
 }
